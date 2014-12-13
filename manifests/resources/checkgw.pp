ffc0864af92fa85f5c83dd16f20302bf68a39b49
ohrensessel <git@leokrueger.de>
2014-12-13 18:54:35 +0100

    new checkgw

--- manifests/resources/checkgw.pp
+++ manifests/resources/checkgw.pp
@@ -2,6 +2,7 @@ class ffnord::resources::checkgw (
   $gw_vpn_interface  = "tun-anonvpn", # Interface name for the anonymous vpn
   $gw_control_ip     = "8.8.8.8",     # Control ip addr
   $gw_bandwidth      = 54,            # How much bandwith we should have up/down per mesh interface
+  $gw_local_ip = "10.112.42.1",                      # a private ip of the gateway
 ) {
 
   file {
@@ -14,6 +15,7 @@ class ffnord::resources::checkgw (
   ffnord::resources::ffnord::field {
     "GW_VPN_INTERFACE": value => "${gw_vpn_interface}";
     "GW_CONTROL_IP": value => "${gw_control_ip}";
+    "GW_LOCAL_IP": value => "${gw_local_ip}";
   }
 
   cron {
